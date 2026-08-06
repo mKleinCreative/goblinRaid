1052b3990cc6de3696af016d5dbe3c05f1ae363c
unknown <miketheklein@gmail.com>
2026-08-05 21:09:24 -0700

    added character updates for ranged, burning buildings and objective clearance

--- gs_step.py
+++ gs_step.py
@@ -1,8 +1,9 @@
 import unreal
 
 les = unreal.get_editor_subsystem(unreal.LevelEditorSubsystem)
+if les.is_in_play_in_editor():
+    les.editor_request_end_play()
+    print('PIE ended (level actors were only moved in PIE - nothing persists)')
 dirty = list(unreal.EditorLoadingAndSavingUtils.get_dirty_map_packages()) + \
         list(unreal.EditorLoadingAndSavingUtils.get_dirty_content_packages())
-print('in PIE:', les.is_in_play_in_editor())
-print('dirty :', [p.get_name() for p in dirty])
-print('SAFE TO CLOSE' if not dirty and not les.is_in_play_in_editor() else 'NOT SAFE')
+print('dirty:', [p.get_name() for p in dirty])
